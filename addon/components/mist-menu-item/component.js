cbbfb27ba79597f3dde1b5791847d11b9d21bc88
Pieterjan C <pieterjan.carly@gmail.com>
2016-05-29 22:04:48 +0200

    Added dependency for ember-simple-auth

--- addon/components/mist-menu-item/component.js
+++ addon/components/mist-menu-item/component.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'li'
+  tagName: 'li',
+  hasAction: Ember.computed('action', function(){
+    return !Ember.isBlank(this.get('action'));
+  }),
+  click() {
+    if(this.get('hasAction')){
+      return this.sendAction();
+    }
+  }
 });
--- app/services/logged-in-user.js
+++ app/services/logged-in-user.js
@@ -1,3 +1,27 @@
+import Ember from 'ember';
+
 export default Ember.Service.extend({
-  user: null
+  session: Ember.inject.service('session'),
+  store: Ember.inject.service(),
+  user: null,
+  isAuthenticated: Ember.computed.oneWay('session.isAuthenticated', function() {
+    return this.get('session.isAuthenticated');
+  }),
+  loadCurrentUser() {
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      const userId = this.get('session.data.authenticated.user_id');
+      if (!Ember.isEmpty(userId)) {
+        return this.get('store').find('user', userId).then((user) => {
+          this.set('user', user);
+          resolve();
+        }, reject);
+      } else {
+        resolve();
+      }
+    });
+  },
+  logOut(){
+    this.set('user', null);
+    this.get('session').invalidate();
+  }
 });
