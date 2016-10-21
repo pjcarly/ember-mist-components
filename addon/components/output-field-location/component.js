dc2c4fb3491d7f82fb5196a5a64204e2ee4632ce
Pieterjan C <pieterjan.carly@gmail.com>
2016-10-21 11:53:21 +0200

    Output location, static GMAPS image

--- addon/components/output-field-location/component.js
+++ addon/components/output-field-location/component.js
@@ -2,5 +2,8 @@ import Ember from 'ember';
 import InputFieldLocation from 'ember-mist-components/components/input-field-location/component';
 
 export default InputFieldLocation.extend({
-  draggable: false
+  key: Ember.computed(function(){
+    let config = Ember.getOwner(this).resolveRegistration('config:environment');
+    return config.googleMap.apiKey;
+  })
 });
