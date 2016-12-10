5aee0a872abb8f979daec4c655b870d955060d98
Pieterjan C <pieterjan.carly@gmail.com>
2016-12-10 02:12:22 +0100

    Addded parent Route to entity router

--- app/services/entity-router.js
+++ app/services/entity-router.js
@@ -1,21 +1,36 @@
 import Ember from 'ember';
-import { getModelName } from 'ember-field-components/classes/model-utils';
+import { getModelName, getParentRoute } from 'ember-field-components/classes/model-utils';
 
 export default Ember.Service.extend({
   // beware, -routing is a private API, migrate once Routing as a Service is released:
   //https://github.com/emberjs/ember.js/issues/12719
   router: Ember.inject.service('-routing'),
   transitionToView: function(model){
-    let modelName = getModelName(model);
-    this.get('router').transitionTo(`${modelName}.view`, [model.get('id')]);
+    const modelName = getModelName(model);
+    const parentRoute = getParentRoute(model);
+    let routePrefix = '';
+    if(!Ember.isBlank(parentRoute)){
+      routePrefix = `${parentRoute}.`;
+    }
+    this.get('router').transitionTo(`${routePrefix}${modelName}.view`, [model.get('id')]);
   },
   transitionToEdit: function(model){
-    let modelName = getModelName(model);
-    this.get('router').transitionTo(`${modelName}.edit`, [model.get('id')]);
+    const modelName = getModelName(model);
+    const parentRoute = getParentRoute(model);
+    let routePrefix = '';
+    if(!Ember.isBlank(parentRoute)){
+      routePrefix = `${parentRoute}.`;
+    }
+    this.get('router').transitionTo(`${routePrefix}${modelName}.edit`, [model.get('id')]);
   },
   transitionToDelete: function(model){
-    let modelName = getModelName(model);
-    this.get('router').transitionTo(`${modelName}.delete`, [model.get('id')]);
+    const modelName = getModelName(model);
+    const parentRoute = getParentRoute(model);
+    let routePrefix = '';
+    if(!Ember.isBlank(parentRoute)){
+      routePrefix = `${parentRoute}.`;
+    }
+    this.get('router').transitionTo(`${routePrefix}${modelName}.delete`, [model.get('id')]);
   },
   transitionToCreate: function(modelType){
     this.get('router').transitionTo(`${modelType}.new`);
