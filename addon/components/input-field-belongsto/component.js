604d043a86af1b9bc2af56b790a8784a7da3ff10
Pieterjan C <pieterjan.carly@gmail.com>
2016-12-14 15:10:00 +0100

    input type select on cacheable belongsto relationships

--- addon/components/input-field-belongsto/component.js
+++ addon/components/input-field-belongsto/component.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
+import DS from 'ember-data';
+import ComponentFieldTypeMixin from 'ember-field-components/mixins/component-field-type';
+
 import ModelUtils from 'ember-field-components/classes/model-utils';
 import { task } from 'ember-concurrency';
 const { camelize } = Ember.String;
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(ComponentFieldTypeMixin, {
   tagName: '',
   store: Ember.inject.service(),
   init(){
@@ -16,11 +19,67 @@ export default Ember.Component.extend({
   setInitialValue: task(function * (){
     let field = this.get('field');
     let model = this.get('model');
-    let store = this.get('store');
 
-    const relationshipType = ModelUtils.getParentModelType(model, field, store);
-    const relationshipTypeName = ModelUtils.getParentModelTypeName(model, field);
+    yield this.get('checkOfflineCache').perform();
 
+    let id = this.get('fieldId');
+    if(!Ember.isBlank(id)){
+      const relationshipTypeName = ModelUtils.getParentModelTypeName(model, field);
+
+      if(this.get('store').hasRecordForId(relationshipTypeName, id)){
+        this.set('lookupValue', this.get('store').peekRecord(relationshipTypeName, id));
+      } else {
+        yield model.get(field).then((value) => {
+          this.set('lookupValue', value);
+        });
+      }
+    }
+
+    if(this.get('isSelect')){
+      yield this.get('setSelectOptions').perform();
+    }
+  }),
+  isRequired: Ember.computed('relationshipAttributeOptions', function(){
+    return this.get('relationshipAttributeOptions').validation.required
+  }),
+  fieldId: Ember.computed('model', 'field', function(){
+    // todo returns blank after clearing field and rolling back attributes (when it should be the initial value)
+    return this.get('model').belongsTo(this.get('field')).id();
+  }),
+  isSelect: Ember.computed(function(){
+    return ModelUtils.hasWidget(this.get('relationshipAttributeOptions'), 'select');
+  }),
+  setSelectOptions: task(function * (){
+    const store = this.get('store');
+    const relationshipType = ModelUtils.getParentModelType(this.get('model'), this.get('field'), store);
+    const relationshipTypeName = ModelUtils.getParentModelTypeName(this.get('model'), this.get('field'));
+
+    let models;
+    if(ModelUtils.modelTypeIsCacheable(relationshipType)){
+      // should have been previously loaded, we can just use peekAll
+      models = store.peekAll(relationshipTypeName);
+    } else {
+      Ember.assert('Select widget is only supported for Offline cacheable models');
+    }
+
+    let selectOptions = [];
+    models.forEach((model) => {
+      let selectOption = {};
+      selectOption.value = model.get('id');
+      selectOption.label = model.get('name');
+      selectOptions.push(selectOption);
+    });
+
+    this.set('selectOptions', selectOptions);
+  }),
+  noChoiceAvailable: Ember.computed('selectOptions', 'value', function(){
+    const selectOptions = this.get('selectOptions');
+    return (selectOptions.get('length') === 1) && selectOptions[0].value === this.get('fieldId');
+  }),
+  checkOfflineCache: task(function * (){
+    const store = this.get('store');
+    const relationshipType = ModelUtils.getParentModelType(this.get('model'), this.get('field'), store);
+    const relationshipTypeName = ModelUtils.getParentModelTypeName(this.get('model'), this.get('field'));
     // first we check if the modelType is cacheable, and if so, lets check the cache, and push them in the store if not yet done so
     if(ModelUtils.modelTypeIsCacheable(relationshipType) && !ModelUtils.modelTypeHasBeenLoadedFromCache(relationshipType)){
       // So the modelType is Cache, and hasn't already been loaded
@@ -53,22 +112,29 @@ export default Ember.Component.extend({
       // and finally we set the cached flag
       ModelUtils.modelTypeHasBeenLoadedFromCache(relationshipType);
     }
-
-    let id = model.belongsTo(field).id(); // todo returns blank after clearing field and rolling back attributes (when it should be the initial value)
-    if(!Ember.isBlank(id)){
-      if(store.hasRecordForId(relationshipTypeName, id)){
-        this.set('lookupValue', store.peekRecord(relationshipTypeName, id));
-      } else {
-        yield model.get(field).then((value) => {
-          this.set('lookupValue', value);
-        });
-      }
-    }
   }),
   actions: {
     valueChanged: function(value){
-      let field = this.get('field');
-      let model = this.get('model');
+      const field = this.get('field');
+      const model = this.get('model');
+
+      if(!Ember.isBlank(value)) {
+        if(!(value instanceof DS.Model)) {
+          // we might have an Id instead of a model, happens when using Select widget
+          const relationshipType = ModelUtils.getParentModelType(model, field, this.get('store'));
+          if(ModelUtils.modelTypeIsCacheable(relationshipType)) {
+            // only usable with cached data
+            // and it should already be available in the store
+            const relationshipTypeName = ModelUtils.getParentModelTypeName(model, field);
+            let foundModel = this.get('store').peekRecord(relationshipTypeName, value);
+            Ember.assert(`Model with id: ${value} and type: ${relationshipTypeName} not found in store`, !Ember.isBlank(foundModel));
+
+            value = foundModel;
+          } else {
+            Ember.assert('Value assign based on ID is only usable with cached models');
+          }
+        }
+      }
 
       model.set(field, value);
       this.set('lookupValue', value);
