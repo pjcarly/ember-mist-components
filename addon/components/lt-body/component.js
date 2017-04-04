4f1bfa757d1f2e75f88a0ffe9a2297126247e416
Pieterjan C <pieterjan.carly@gmail.com>
2017-04-04 14:03:53 +0200

    Bugfix

--- addon/components/lt-body/component.js
+++ addon/components/lt-body/component.js
@@ -1,13 +1,12 @@
 import Body from 'ember-light-table/components/lt-body';
-import callAction from 'ember-light-table/utils/call-action';
 
 export default Body.extend({
   actions: {
     onRowMouseEnter(/*row*/) {
-      callAction(this, 'onRowMouseEnter', ...arguments);
+      this.sendAction('onRowMouseEnter', ...arguments);
     },
     onRowMouseLeave(/*row*/) {
-      callAction(this, 'onRowMouseLeave', ...arguments);
+      this.sendAction('onRowMouseLeave', ...arguments);
     }
   }
 });
