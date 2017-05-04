67dbdff8b8dd07b7d531140d0525bd047d423669
Pieterjan C <pieterjan.carly@gmail.com>
2017-05-04 10:43:13 +0200

    Fix address update

--- addon/mixins/model-attributes.js
+++ addon/mixins/model-attributes.js
@@ -2,33 +2,53 @@ import Ember from 'ember';
 import Location from 'ember-mist-components/classes/location';
 import Address from 'ember-mist-components/classes/address';
 
-const { Mixin, on } = Ember;
+const { Mixin, on, computed } = Ember;
 
 export default Mixin.create({
-  setDefaultValuesForAttributes: on('ready', function(){
+  setDefaultValuesForAttributes: on('didLoad', 'didUpdate', function(){
     this.eachAttribute((name, attribute) => {
       if(attribute.type === 'location'){
+        const currentValue = this.get(name);
+
         let locations = this.get('locations');
-        if(locations === undefined){
-          locations = {};
-          this.set('locations', locations);
-        }
         let location = Location.create();
         location.model = this;
         location.field = name;
+        location.set('lat', currentValue.hasOwnProperty('lat') ? currentValue.lat : null);
+        location.set('lng', currentValue.hasOwnProperty('lng') ? currentValue.lng : null);
         locations[name] = location;
       } else if(attribute.type === 'address'){
+        const currentValue = this.get(name);
         let addresses = this.get('addresses');
-        if(addresses === undefined){
-          addresses = {};
-          this.set('addresses', addresses);
-        }
         let address = Address.create();
         address.model = this;
         address.field = name;
+        address.set('street', currentValue.hasOwnProperty('street') ? currentValue.street : null);
+        address.set('city', currentValue.hasOwnProperty('city') ? currentValue.city : null);
+        address.set('postalCode', currentValue.hasOwnProperty('postalCode') ? currentValue.postalCode : null);
+        address.set('state', currentValue.hasOwnProperty('state') ? currentValue.state : null);
+        address.set('country', currentValue.hasOwnProperty('country') ? currentValue.country : null);
         addresses[name] = address;
       }
     });
+  }),
+
+  locations: computed('_locations', function(){
+    let locations = this.get('_locations');
+    if(locations === undefined){
+      locations = {};
+      this.set('_locations', locations);
+    }
+    return locations;
+  }),
+
+  addresses: computed('_addresses', function(){
+    let addresses = this.get('_addresses');
+    if(addresses === undefined){
+      addresses = {};
+      this.set('_addresses', addresses);
+    }
+    return addresses;
   }),
 
   getLocation(name){
