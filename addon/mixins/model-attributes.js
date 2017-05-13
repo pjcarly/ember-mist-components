598ce0e5feb663494b187e28b8af5c0ede773e42
Pieterjan C <pieterjan.carly@gmail.com>
2017-05-14 01:37:46 +0200

    fix empty value and on trigger model attributes

--- addon/mixins/model-attributes.js
+++ addon/mixins/model-attributes.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 import Location from 'ember-mist-components/classes/location';
 import Address from 'ember-mist-components/classes/address';
 
-const { Mixin, on, computed } = Ember;
+const { Mixin, on, computed, isBlank } = Ember;
 
 export default Mixin.create({
-  setDefaultValuesForAttributes: on('didLoad', 'didUpdate', function(){
+  setDefaultValuesForAttributes: on('ready', function(){
     this.eachAttribute((name, attribute) => {
       if(attribute.type === 'location'){
         const currentValue = this.get(name);
@@ -14,8 +14,8 @@ export default Mixin.create({
         let location = Location.create();
         location.model = this;
         location.field = name;
-        location.set('lat', currentValue.hasOwnProperty('lat') ? currentValue.lat : null);
-        location.set('lng', currentValue.hasOwnProperty('lng') ? currentValue.lng : null);
+        location.set('lat', (!isBlank(currentValue) && currentValue.hasOwnProperty('lat')) ? currentValue.lat : null);
+        location.set('lng', (!isBlank(currentValue) && currentValue.hasOwnProperty('lng')) ? currentValue.lng : null);
         locations[name] = location;
       } else if(attribute.type === 'address'){
         const currentValue = this.get(name);
@@ -23,11 +23,11 @@ export default Mixin.create({
         let address = Address.create();
         address.model = this;
         address.field = name;
-        address.set('street', currentValue.hasOwnProperty('street') ? currentValue.street : null);
-        address.set('city', currentValue.hasOwnProperty('city') ? currentValue.city : null);
-        address.set('postalCode', currentValue.hasOwnProperty('postalCode') ? currentValue.postalCode : null);
-        address.set('state', currentValue.hasOwnProperty('state') ? currentValue.state : null);
-        address.set('country', currentValue.hasOwnProperty('country') ? currentValue.country : null);
+        address.set('street', (!isBlank(currentValue) && currentValue.hasOwnProperty('street')) ? currentValue.street : null);
+        address.set('city', (!isBlank(currentValue) && currentValue.hasOwnProperty('city')) ? currentValue.city : null);
+        address.set('postalCode', (!isBlank(currentValue) && currentValue.hasOwnProperty('postalCode')) ? currentValue.postalCode : null);
+        address.set('state', (!isBlank(currentValue) && currentValue.hasOwnProperty('state')) ? currentValue.state : null);
+        address.set('country', (!isBlank(currentValue) && currentValue.hasOwnProperty('country')) ? currentValue.country : null);
         addresses[name] = address;
       }
     });
