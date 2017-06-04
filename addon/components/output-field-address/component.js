fdf0989c9d40ae7bf9e39bc82757a2bdeff136c0
Pieterjan C <pieterjan.carly@gmail.com>
2017-06-04 03:12:11 +0200

    Output Field Address

--- addon/components/output-field-address/component.js
+++ addon/components/output-field-address/component.js
@@ -1,13 +1,26 @@
 import Ember from 'ember';
-import { getCountrySelectOptions } from 'ember-mist-components/classes/address';
-const { Component, computed } = Ember;
+import InputAddress from 'ember-mist-components/components/input-field-address/component';
+import { replaceAll } from 'ember-field-components/classes/utils';
 
-export default Component.extend({
-  tagName: '',
-  countrySelectOptions: computed(function(){
-    return getCountrySelectOptions();
-  }),
-  value: computed('model', 'field', function(){
-    return this.get('model').get(this.get('field'));
+const { computed, isBlank } = Ember;
+
+export default InputAddress.extend({
+  outputDisplayRows: computed('displayRows', function(){
+    const { displayRows, address } = this.getProperties('displayRows', 'address');
+    let outputDisplayRows = [];
+
+    for(let row of displayRows) {
+      let emptyRow = false;
+      for(let column of row.columns) {
+        column.component = replaceAll(column.component, 'input', 'output');
+
+        emptyRow = isBlank(address.get(column.field)) || emptyRow;
+      }
+
+      row.emptyRow = emptyRow;
+      outputDisplayRows.push(row);
+    }
+
+    return outputDisplayRows;
   })
 });
