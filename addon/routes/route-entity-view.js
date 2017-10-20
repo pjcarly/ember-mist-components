74633b983af4c1a505af05aadf9fca08aafe7cdf
Pieterjan Carly <pieterjan.carly@gmail.com>
2017-10-20 23:39:56 +0200

    Added recently viewed component

--- addon/routes/route-entity-view.js
+++ addon/routes/route-entity-view.js
@@ -2,13 +2,49 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import SingleEntityRouteMixin from 'ember-mist-components/mixins/route-single-entity';
 import ScrollToTop from 'ember-mist-components/mixins/route-scroll-to-top';
+import { getModelName } from 'ember-field-components/classes/model-utils';
 
-const { Route, inject } = Ember;
+const { Route } = Ember;
+const { isBlank } = Ember;
+const { inject } = Ember;
 const { service } = inject;
 
 export default Route.extend(AuthenticatedRouteMixin, SingleEntityRouteMixin, ScrollToTop, {
   entityCache: service(),
-  afterModel(){
+  storage: service(),
+  afterModel(model){
     this.get('entityCache').clearReturnToModel();
+    this.addRecentlyViewed(model);
+  },
+  addRecentlyViewed(model){
+    const storage = this.get('storage');
+
+    let newRecentlyViewedRecord = {
+      type: getModelName(model),
+      name: model.get('name'),
+      id: model.get('id')
+    };
+
+    let oldRecentlyViewedRecords = storage.get('recentlyViewedRecords');
+    if(isBlank(oldRecentlyViewedRecords)){
+      oldRecentlyViewedRecords = [];
+    }
+
+    let newRecentlyViewedRecords = [];
+    newRecentlyViewedRecords.push(newRecentlyViewedRecord);
+
+    let index = 1;
+    for(let oldRecentlyViewedRecord of oldRecentlyViewedRecords){
+      if(!(oldRecentlyViewedRecord.id === newRecentlyViewedRecord.id && oldRecentlyViewedRecord.type === newRecentlyViewedRecord.type)){
+        newRecentlyViewedRecords.push(oldRecentlyViewedRecord);
+        index++;
+
+        if(index >= 10) {
+          break;
+        }
+      }
+    }
+
+    storage.set('recentlyViewedRecords', newRecentlyViewedRecords);
   }
 });
