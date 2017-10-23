5e18ca9497247827864e8fb7fa026f8d0e19f415
Pieterjan Carly <pieterjan.carly@gmail.com>
2017-10-23 10:16:47 +0200

    Migrated recently viewed to href-to

--- addon/components/mist-recently-viewed/component.js
+++ addon/components/mist-recently-viewed/component.js
@@ -10,9 +10,6 @@ const { service } = inject;
 export default Component.extend({
   classNames: ['card recently-viewed'],
   storage: service(),
-  store: service(),
-  entityRouter: service(),
-  toast: service(),
   recentlyViewedRecords: computed('storage.recentlyViewedRecords', function(){
     const recentlyViewedRecords = this.get('storage.recentlyViewedRecords');
     if(isBlank(recentlyViewedRecords)){
@@ -20,28 +17,5 @@ export default Component.extend({
     } else {
       return recentlyViewedRecords;
     }
-  }),
-  navigateTo: task(function * (record){
-    yield this.get('store').findRecord(record.type, record.id).then((foundRecord) => {
-      this.get('entityRouter').transitionToView(foundRecord);
-    }).catch(() => {
-      // remove record from recentlyViewed, it is no longer available, deleted possibly?
-      this.get('toast').warning('Record not found', 'Record not found');
-      const oldRecentlyViewedRecords = this.get('storage.recentlyViewedRecords');
-      let newRecentlyViewedRecords = [];
-
-      for(const oldRecentlyViewedRecord of oldRecentlyViewedRecords){
-        if(!(oldRecentlyViewedRecord.id === record.id && oldRecentlyViewedRecord.type === record.type)){
-          newRecentlyViewedRecords.push(oldRecentlyViewedRecord);
-        }
-      }
-
-      this.set('storage.recentlyViewedRecords', newRecentlyViewedRecords);
-    });
-  }),
-  actions: {
-    recordSelected(record){
-      this.get('navigateTo').perform(record);
-    }
-  }
+  })
 })
