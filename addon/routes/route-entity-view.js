35faa55996af20f0d255a2d76d39c502e5e38c56
Pieterjan Carly <pieterjan.carly@gmail.com>
2017-11-01 17:30:12 +0100

    Added remove recently viewed record for records failing to load

--- addon/routes/route-entity-view.js
+++ addon/routes/route-entity-view.js
@@ -12,9 +12,46 @@ const { service } = inject;
 export default Route.extend(AuthenticatedRouteMixin, SingleEntityRouteMixin, ScrollToTop, {
   entityCache: service(),
   storage: service(),
+  model(params){
+    return this._super(...arguments).catch((error) => {
+      const entityType = this.get('entityName');
+      if(!isBlank(entityType)) {
+        const idParam = `${entityType}_id`;
+        if(!isBlank(idParam) && params.hasOwnProperty(idParam)) {
+          const id = params[idParam];
+          if(!isBlank(id)) {
+            this.removeRecentlyViewed(entityType, id);
+          }
+        }
+      }
+    });
+  },
   afterModel(model){
     this.get('entityCache').clearReturnToModel();
     this.addRecentlyViewed(model);
+  },
+  removeRecentlyViewed(type, id){
+    const storage = this.get('storage');
+
+    let oldRecentlyViewedRecords = storage.get('recentlyViewedRecords');
+    if(isBlank(oldRecentlyViewedRecords)){
+      oldRecentlyViewedRecords = [];
+    }
+
+    let newRecentlyViewedRecords = [];
+    let index = 1;
+    for(let oldRecentlyViewedRecord of oldRecentlyViewedRecords){
+      if(!(oldRecentlyViewedRecord.id === id && oldRecentlyViewedRecord.type === type)){
+        newRecentlyViewedRecords.push(oldRecentlyViewedRecord);
+        index++;
+
+        if(index >= 10) {
+          break;
+        }
+      }
+    }
+
+    storage.set('recentlyViewedRecords', newRecentlyViewedRecords);
   },
   addRecentlyViewed(model){
     const storage = this.get('storage');
