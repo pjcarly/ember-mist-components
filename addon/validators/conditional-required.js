c84f597d8ec1362d5afc3dd64d6743e944d83115
Pieterjan C <pieterjan.carly@gmail.com>
2018-08-24 15:21:24 +0200

    Conditional Required for BelongsTo

--- addon/validators/conditional-required.js
+++ addon/validators/conditional-required.js
@@ -7,8 +7,14 @@ export default Validator.extend({
   validate(name, value, attribute, model) {
     const requiredField = attribute.options.validation.conditionalRequired;
 
-    if(model.get(requiredField) && isBlank(value)){
-      return this.format();
+    if(attribute.isAttribute) {
+      if(model.get(requiredField) && isBlank(value)){
+        return this.format();
+      }
+    } else if (attribute.isRelationship && attribute.kind === 'belongsTo') {
+      if(model.get(requiredField) && isBlank(model.belongsTo(name).id())){
+        return this.format();
+      }
     }
   }
 });
