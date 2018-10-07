2a69db6f3434c5d3962ea5893b0a93dbcc4e4a0f
Pieterjan C <pieterjan.carly@gmail.com>
2018-10-08 00:26:26 +0200

    Refactored loggedInUser service

--- app/services/logged-in-user.js
+++ app/services/logged-in-user.js
@@ -2,27 +2,23 @@ import Service from '@ember/service';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 import { isBlank } from '@ember/utils';
-import { Promise } from 'rsvp';
 
 export default Service.extend({
-  session: service('session'),
+  session: service(),
   store: service(),
-  user: null,
   isAuthenticated: computed.oneWay('session.isAuthenticated', function() {
     return this.get('session.isAuthenticated');
   }),
   loadCurrentUser() {
-    return new Promise((resolve, reject) => {
-      const userId = this.get('session.data.authenticated.user_id');
-      if (!isBlank(userId)) {
-        return this.get('store').find('user', userId).then((user) => {
-          this.set('user', user);
-          resolve();
-        }, reject);
-      } else {
-        resolve();
-      }
-    });
+    const userId = this.get('session.data.authenticated.user_id');
+
+    if(!isBlank(userId)){
+      return this.get('store').loadRecord('user', userId)
+      .then((user) => {
+        this.set('user', user);
+        return user;
+      });
+    }
   },
   logOut(){
     this.set('user', null);
