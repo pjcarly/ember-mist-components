96e26125e452f38db9da92d23c8c1703b9456f82
Pieterjan C <pieterjan.carly@gmail.com>
2018-11-01 12:06:14 +0100

    import change

--- addon/mixins/entity-drupal.js
+++ addon/mixins/entity-drupal.js
@@ -1,11 +1,11 @@
 import Mixin from '@ember/object/mixin';
-import Attribute from 'ember-field-components/classes/attribute';
+import { setType } from 'ember-field-components/classes/attribute';
 
 export default Mixin.create({
-  created: Attribute.setType('datetime', {
+  created: setType('datetime', {
     readOnly: true
   }),
-  changed: Attribute.setType('datetime', {
+  changed: setType('datetime', {
     readOnly: true
   })
 });
