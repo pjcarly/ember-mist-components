f5efa78c9484030d15f7188b2255dfc6b1c5b25a
Pieterjan C <pieterjan.carly@gmail.com>
2019-03-02 00:51:08 +0100

    exported ajax service

---
+++ addon/services/ajax.js
@@ -0,0 +1,27 @@
+import AjaxService from 'ember-ajax/services/ajax';
+import { computed } from '@ember/object';
+import { isBlank } from '@ember/utils';
+import { inject as service } from '@ember/service';
+import { getOwner } from '@ember/application';
+
+export default AjaxService.extend({
+  session: service(),
+  host: computed(function(){
+    let config = getOwner(this).resolveRegistration('config:environment');
+    return config.apiHost;
+  }),
+  endpoint: computed(function(){
+    let config = getOwner(this).resolveRegistration('config:environment');
+    return config.apiEndpoint;
+  }),
+  headers: computed('session.data.authenticated.access_token', function(){
+    const headers = {};
+    const access_token = this.get('session.data.authenticated.access_token');
+
+    if(!isBlank(access_token)){
+      headers['Authorization'] = `Bearer ${access_token}`;
+    }
+
+    return headers;
+  })
+});
--- app/services/ajax.js
+++ app/services/ajax.js
@@ -1,27 +1,3 @@
-import AjaxService from 'ember-ajax/services/ajax';
-import { computed } from '@ember/object';
-import { isBlank } from '@ember/utils';
-import { inject as service } from '@ember/service';
-import { getOwner } from '@ember/application';
+import service from 'ember-mist-components/services/ajax';
 
-export default AjaxService.extend({
-  session: service(),
-  host: computed(function(){
-    let config = getOwner(this).resolveRegistration('config:environment');
-    return config.apiHost;
-  }),
-  endpoint: computed(function(){
-    let config = getOwner(this).resolveRegistration('config:environment');
-    return config.apiEndpoint;
-  }),
-  headers: computed('session.data.authenticated.access_token', function(){
-    const headers = {};
-    const access_token = this.get('session.data.authenticated.access_token');
-
-    if(!isBlank(access_token)){
-      headers['Authorization'] = `Bearer ${access_token}`;
-    }
-
-    return headers;
-  })
-});
+export default service;
