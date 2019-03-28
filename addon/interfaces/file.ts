184c5851badd568522b63ac70af6b09c1c5b888d
Pieterjan C <pieterjan.carly@gmail.com>
2019-03-28 11:34:36 +0100

    added getRecentlyViewed records, and Image and File interface

---
+++ addon/interfaces/file.ts
@@ -0,0 +1,8 @@
+export default interface File {
+  id : string;
+  url : string;
+  hash : string;
+  filesize : number;
+  filename : string;
+  filemime : string;
+}
--- addon/services/recently-viewed.ts
+++ addon/services/recently-viewed.ts
@@ -3,6 +3,13 @@ import DrupalModel from "ember-mist-components/models/drupal-model";
 import FieldInformationService from "ember-field-components/app/services/field-information";
 import { inject as service } from '@ember-decorators/service';
 import { isBlank } from '@ember/utils';
+import { computed } from "@ember-decorators/object";
+
+export interface RecentlyViewedRecord {
+  type : string;
+  name : string;
+  id : string;
+}
 
 export default class RecentlyViewedService extends Service {
   @service storage !: any;
@@ -14,12 +21,9 @@ export default class RecentlyViewedService extends Service {
    * @param id THe ID of the model you want to remove from recently viewed
    */
   removeRecentlyViewed(type : string, id : string) : void {
-    let oldRecentlyViewedRecords = this.storage.get('recentlyViewedRecords');
-    if(isBlank(oldRecentlyViewedRecords)){
-      oldRecentlyViewedRecords = [];
-    }
+    const oldRecentlyViewedRecords = this.records;
 
-    let newRecentlyViewedRecords = [];
+    let newRecentlyViewedRecords : RecentlyViewedRecord[] = [];
     let index = 1;
     for(let oldRecentlyViewedRecord of oldRecentlyViewedRecords){
       if(!(oldRecentlyViewedRecord.id === id && oldRecentlyViewedRecord.type === type)){
@@ -47,10 +51,7 @@ export default class RecentlyViewedService extends Service {
         id: model.id
       };
 
-      let oldRecentlyViewedRecords = this.storage.get('recentlyViewedRecords');
-      if(isBlank(oldRecentlyViewedRecords)){
-        oldRecentlyViewedRecords = [];
-      }
+      const oldRecentlyViewedRecords = this.records;
 
       let newRecentlyViewedRecords = [];
       newRecentlyViewedRecords.push(newRecentlyViewedRecord);
@@ -69,5 +70,18 @@ export default class RecentlyViewedService extends Service {
 
       this.storage.set('recentlyViewedRecords', newRecentlyViewedRecords);
     }
+  }
+
+  /**
+   * Returns the Recently Viewed records currently in local storage, if nothing is found an empty array is returned
+   */
+  @computed('storage.recentlyViewedRecords.[]')
+  get records() : RecentlyViewedRecord[] {
+    let oldRecentlyViewedRecords : RecentlyViewedRecord[] = this.storage.get('recentlyViewedRecords');
+    if(isBlank(oldRecentlyViewedRecords)){
+      oldRecentlyViewedRecords = [];
+    }
+
+    return oldRecentlyViewedRecords;
   }
 }
