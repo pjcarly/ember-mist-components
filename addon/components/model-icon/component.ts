48013980586cf270d0faeb00ee8e689db329348e
Pieterjan C <pieterjan.carly@gmail.com>
2019-04-11 14:39:44 +0200

    dependencies & moved components

---
+++ addon/components/model-icon/component.ts
@@ -0,0 +1,5 @@
+import Component from "@ember/component";
+
+export default class ModelIconComponent extends Component {
+
+}
--- addon/components/recently-viewed/component.ts
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-import RecentlyViewedService from 'dummy/services/recently-viewed';
-import { inject as service } from '@ember-decorators/service';
-import { tagName } from '@ember-decorators/component';
-
-@tagName('')
-export default class RecentlyViewedComponent extends Component {
-  @service recentlyViewed !: RecentlyViewedService;
-}
