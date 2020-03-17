38182a94e99f0ec3fa3a125529ebfdc5868b8aa5
Pieterjan C <pieterjan.carly@gmail.com>
2020-03-17 17:06:44 +0100

    Added blank template for contextual components

---
+++ addon/components/blank-template/component.ts
@@ -0,0 +1,8 @@
+import Component from "@ember/component";
+import { tagName } from "@ember-decorators/component";
+
+// This template is needed when using Contextual Components
+// Contextual components give the possibility of multiple yields in a component
+
+@tagName("")
+export default class BlankTemplateComponent extends Component {}
