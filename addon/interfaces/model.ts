617e6789d6acf6ed8f2547a7dfe16076a27168ee
Pieterjan C <pieterjan.carly@gmail.com>
2020-04-01 19:09:18 +0200

    type fix and extra class

--- addon/interfaces/model.ts
+++ addon/interfaces/model.ts
@@ -1,6 +1,9 @@
+import DS from "ember-data";
+
 export default interface ModelInterface {
   id: number | string;
   set(property: string, value: any): void;
   get(property: string): any;
   validate(): boolean;
+  errors: DS.Errors;
 }
