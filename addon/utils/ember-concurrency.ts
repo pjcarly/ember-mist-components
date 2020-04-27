04edc4151e327e165bdf4d409d11d09d3c84896d
Pieterjan <pieterjan.carly@gmail.com>
2020-04-27 17:58:19 +0200

    ember-concurrency helper

---
+++ addon/utils/ember-concurrency.ts
@@ -0,0 +1,11 @@
+import Task from "ember-concurrency/task";
+
+type ECTask<Args extends Array<any>, Return> = (
+  ...args: Args
+) => Generator<any /* potentially yielded types */, Return, unknown>;
+
+export function taskFor<Args extends any[], Return = void>(
+  generatorFn: ECTask<Args, Return>
+) {
+  return (generatorFn as any) as Task<Args, Return>;
+}
