79f127e9f76a57b8119ba8110acdb584ff460f96
Pieterjan <pieterjan.carly@gmail.com>
2020-05-10 03:02:34 +0200

    Started Modal component

--- addon/components/modal/component.ts
+++ addon/components/modal/component.ts
@@ -1,18 +1,93 @@
 import Component from "@ember/component";
-import { action } from "@ember/object";
+import { action, computed } from "@ember/object";
 import { tagName } from "@ember-decorators/component";
 // @ts-ignore
 import bsn from "bootstrap.native/dist/bootstrap-native-v4";
+import { guidFor } from "@ember/object/internals";
 
 @tagName("")
 export default class ModalComponent extends Component {
-  modalVisible: boolean = true;
-  modalDisplay?: string;
+  modalVisible: boolean = false;
   modal?: bsn.Modal;
+
+  // didInsertElement() {
+  //   super.didInsertElement();
+  //   const element = document.getElementById("ember-mist-modal-wormhole");
+
+  //   if (element) {
+  //     element.addEventListener(
+  //       "shown.bs.modal",
+  //       (event) => {
+  //         console.log(event);
+  //       },
+  //       false
+  //     );
+  //     element.addEventListener(
+  //       "hidden.bs.modal",
+  //       (event) => {
+  //         console.log(event);
+  //       },
+  //       false
+  //     );
+  //   }
+  // }
+
+  // willDestroyElement() {
+  //   const element = document.getElementById("ember-mist-modal-wormhole");
+
+  //   if (element) {
+  //     element.removeEventListener(
+  //       "show.bs.modal",
+  //       (event) => {
+  //         console.log(event);
+  //       },
+  //       false
+  //     );
+  //     element.removeEventListener(
+  //       "hidden.bs.modal",
+  //       (event) => {
+  //         console.log(event);
+  //       },
+  //       false
+  //     );
+  //   }
+
+  //   super.willDestroyElement();
+  // }
+
+  @computed()
+  get modalId(): string {
+    return `${guidFor(this)}-modal`;
+  }
+
+  @computed("modalId")
+  get modalBodyId(): string {
+    return `${this.modalId}-body`;
+  }
+
+  @computed("modalId")
+  get modalHeaderId(): string {
+    return `${this.modalId}-header`;
+  }
+
+  @computed("modalId")
+  get modalFooterId(): string {
+    return `${this.modalId}-footer`;
+  }
+
+  @computed("modalId")
+  get modalTriggerId(): string {
+    return `${this.modalId}-trigger`;
+  }
+
+  @computed("modalVisible")
+  get status(): string {
+    return this.modalVisible ? "visible" : "hidden";
+  }
 
   getModal() {
     if (!this.modal) {
-      const element = document.getElementById("quote-modal");
+      const element = document.getElementById(this.modalId);
       const modal = new bsn.Modal(element, {});
       this.set("modal", modal);
     }
@@ -22,23 +97,13 @@ export default class ModalComponent extends Component {
 
   @action
   closeModal() {
+    this.set("modalVisible", false);
     this.getModal().hide();
   }
 
   @action
   showModal() {
+    this.set("modalVisible", true);
     this.getModal().show();
-  }
-
-  @action
-  acceptQuote() {
-    this.set("modalDisplay", "quote-accept");
-    this.showModal();
-  }
-
-  @action
-  rejectQuote() {
-    this.set("modalDisplay", "quote-reject");
-    this.showModal();
   }
 }
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  name: require('./package').name,
+  name: require("./package").name,
   isDevelopingAddon() {
     return true;
-  }
+  },
+  contentFor(type, config) {
+    if (config.environment !== "test" && type === "body-footer") {
+      return '<div id="ember-mist-modal-wormhole"></div>';
+    }
+  },
 };
