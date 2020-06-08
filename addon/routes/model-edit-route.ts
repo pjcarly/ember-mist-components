bffa5cbf94bd4407fabd389640f8ee10eaa0e7d8
Pieterjan C <pieterjan.carly@gmail.com>
2020-06-08 14:16:19 +0200

    bugfix editroute

--- addon/routes/model-edit-route.ts
+++ addon/routes/model-edit-route.ts
@@ -1,9 +1,9 @@
-import Route from "@ember/routing/route";
 import Transition from "@ember/routing/-private/transition";
 import { inject as service } from "@ember/service";
 import SessionService from "ember-simple-auth/services/session";
+import SingleModelRoute from "./single-model-route";
 
-export default class ModelEditRoute extends Route {
+export default class ModelEditRoute extends SingleModelRoute {
   @service session!: SessionService;
 
   beforeModel(transition: Transition) {
