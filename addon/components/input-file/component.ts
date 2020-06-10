30fa7ce7eddd4b69203da72042ede6e271b38549
Pieterjan C <pieterjan.carly@gmail.com>
2020-06-10 17:57:33 +0200

    computed inputId for inputfile queue name

--- addon/components/input-file/component.ts
+++ addon/components/input-file/component.ts
@@ -1,11 +1,18 @@
 import BaseInput from "ember-field-components/components/BaseInput";
+import File from "ember-file-upload/file";
 import { isEmpty } from "@ember/utils";
 import { action } from "@ember/object";
-import File from "ember-file-upload/file";
+import { computed } from "@ember/object";
+import { guidFor } from "@ember/object/internals";
 
 export default class InputFileComponent extends BaseInput {
   type = "file";
   multiple: boolean = false;
+
+  @computed("inputId")
+  get inputIdComputed() {
+    return this.inputId ?? `${guidFor(this)}-queue`;
+  }
 
   @action
   filesSelected(file: File) {
