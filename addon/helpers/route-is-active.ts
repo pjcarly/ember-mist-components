88ac0e3a81be6b271491be2d1bb27d9a39eea01f
Pieterjan C <pieterjan.carly@gmail.com>
2020-06-26 16:55:09 +0200

    route-is-active changes

--- addon/helpers/route-is-active.ts
+++ addon/helpers/route-is-active.ts
@@ -5,13 +5,21 @@ import RouterService from "@ember/routing/router-service";
 export default class RouteIsActiveHelper extends Helper {
   @service router!: RouterService;
 
-  compute(
-    [route]: [string],
-    { activeWhen }: { activeWhen: string; value: string }
-  ): boolean {
+  compute([route]: [string], { activeWhen }: { activeWhen?: string }): boolean {
+    const rootRoute = this.getRootRoute(route);
+    const activeRootNode = this.getRootRoute(this.router.currentRouteName);
+
     return (
-      this.router.currentRouteName === route ||
-      this.router.currentRouteName === activeWhen
+      rootRoute === activeRootNode ||
+      (activeWhen !== undefined &&
+        activeWhen !== null &&
+        activeWhen.split(" ").some((possibleActiveRoute) => {
+          return this.getRootRoute(possibleActiveRoute) === rootRoute;
+        }))
     );
+  }
+
+  getRootRoute(route: string): string {
+    return route.split(".")[0];
   }
 }
