17b4baa6c9001d73d712b2157522addfabebdea9
Pieterjan C <pieterjan.carly@gmail.com>
2020-10-05 16:17:30 +0200

    Bootstrap native Carousel

--- addon/components/output-field-images/component.ts
+++ addon/components/output-field-images/component.ts
@@ -2,9 +2,19 @@ import OutputFieldComponent from "@getflights/ember-field-components/components/
 import Image from "@getflights/ember-mist-components/interfaces/image";
 import { computed, action } from "@ember/object";
 import { guidFor } from "@ember/object/internals";
+// @ts-ignore
+import bsn from "bootstrap.native/dist/bootstrap-native-v4";
 
 export default class OutputFieldImagesComponent extends OutputFieldComponent {
   imageClicked(_: Image) {}
+  carousel!: bsn.Carousel;
+
+  didInsertElement() {
+    // @ts-ignore
+    super.didInsertElement(...arguments);
+    const carousel = new bsn.Carousel(`#${this.carouselName}`);
+    this.set("carousel", carousel);
+  }
 
   @computed()
   get carouselName(): string {
