982423df693141808265b0e8c3f418b860271a4d
Pieterjan C <pieterjan.carly@gmail.com>
2020-11-28 10:50:14 +0100

    added default validations per type

--- addon/decorators/attribute.ts
+++ addon/decorators/attribute.ts
@@ -1,11 +1,26 @@
-import { attr } from "@ember-data/model";
+import Model, { attr } from "@ember-data/model";
+import { getOwner } from "@ember/application";
+import FieldInformationService from "@getflights/ember-field-components/addon/services/field-information";
 // @ts-ignore
 import { fragment } from "ember-data-model-fragments/attributes";
 
-export function field(type: string, _options?: any) {
+export function field(type: string, options?: any) {
   if (type === "address") {
     return fragment(...arguments);
   } else {
+    if (type === "currency") {
+      if (!options) {
+        options = {};
+      }
+
+      options.defaultValue = (model: Model) => {
+        const fieldInformation: FieldInformationService = getOwner(
+          model
+        ).lookup("service:field-information");
+        return fieldInformation.defaultCurrency;
+      };
+    }
+
     return attr(...arguments);
   }
 }
