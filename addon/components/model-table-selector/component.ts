f1241bcfedff43944d9a202af30dd3a4dacdf7d6
Pieterjan C <pieterjan.carly@gmail.com>
2021-03-01 16:13:35 +0100

    Removed ModelTable

--- addon/components/model-table-selector/component.ts
+++
@@ -1,9 +0,0 @@
-import Component from "@glimmer/component";
-import { Column, Row } from "../model-table/component";
-
-interface Arguments {
-  row: Row;
-  column: Column;
-}
-
-export default class ModelTableSelectorComponent extends Component<Arguments> {}
