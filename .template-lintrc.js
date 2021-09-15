3ea7717f44ab140c0a09f60287f9a0e618587e9a
Pieterjan C <pieterjan.carly@gmail.com>
2021-09-15 15:45:57 +0200

    typed templates

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,7 +1,11 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended',
+  plugins: ['ember-template-lint-typed-templates'],
+  extends: [
+    'octane', // this comes from ember-template-lint
+    'ember-template-lint-typed-templates:recommended'
+  ],
   rules: {
     'no-bare-strings': false,
   },
